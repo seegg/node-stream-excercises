23df574517b156216dde68773e89c0b6117753fd
yuan tan <dyxtan@gmail.com>
2022-01-27 03:31:02 +1300

    finish html stream

--- e12-html-stream.js
+++ e12-html-stream.js
@@ -1,2 +1,21 @@
 const trumpet = require('trumpet');
 const through = require('through2');
+
+
+const trp = trumpet();
+const tr = through(write, end);
+
+function write (buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end (done) {
+  done();
+}
+
+process.stdin.pipe(trp);
+trp.pipe(process.stdout);
+
+const stream = trp.select('.loud').createStream();
+stream.pipe(tr).pipe(stream);
